37f48bbc9adeb211f518b3093bb064346a471e3e
pandukawedisinghe <panduka@aleph-labs.com>
2021-02-11 15:12:46 +0800

    Add Movie model to data package

---
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -0,0 +1,36 @@
+package com.example.movieapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("Title") val title: String,
+    @SerializedName("Year") val year: String,
+    @SerializedName("Rated") val rated: String,
+    @SerializedName("Released") val released: String,
+    @SerializedName("Runtime") val runtime: String,
+    @SerializedName("Genre") val genre: String,
+    @SerializedName("Director") val director: String,
+    @SerializedName("Writer") val writer: String,
+    @SerializedName("Actors") val actors: String,
+    @SerializedName("Plot") val plot: String,
+    @SerializedName("Language") val language: String,
+    @SerializedName("Country") val country: String,
+    @SerializedName("Awards") val awards: String,
+    @SerializedName("Poster") val poster: String,
+    @SerializedName("Ratings") val ratings: List<Rating>,
+    @SerializedName("Metascore") val metascore: String,
+    @SerializedName("Type") val type: String,
+    @SerializedName("DVD") val dvd: String,
+    @SerializedName("BoxOffice") val boxOffice: String,
+    @SerializedName("Production") val production: String,
+    @SerializedName("Website") val website: String,
+    @SerializedName("Response") val response: String,
+    val imdbRating: String,
+    val imdbVotes: String,
+    val imdbID: String
+)
+
+data class Rating(
+    @SerializedName("Source") val source: String,
+    @SerializedName("Value") val value: String
+)
